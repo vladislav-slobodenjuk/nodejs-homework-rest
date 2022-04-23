740b3f0453a078092e2b6bc9d37d3ad145189211
vladislav-slobodenjuk <vladjun@gmail.com>
2022-04-23 16:28:21 +0300

    updt contactSchema

--- config/db.js
+++ config/db.js
@@ -3,26 +3,24 @@ require("dotenv").config();
 
 const DB_HOST = process.env.DB_HOST;
 
-const db = mongoose
-  .connect(DB_HOST)
-  .then(console.log("Database connection successful"));
+const db = mongoose.connect(DB_HOST);
 
 // const db = mongoose.connect(uri, {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
 // });
 
-// mongoose.connection.on("connected", () => {
-//   console.log("Connected to DB");
-// });
+mongoose.connection.on("connected", () => {
+  console.log("Database connection successful");
+});
 
-// mongoose.connection.on("error", (err) => {
-//   console.log(`Mongoose connection error: ${err}`);
-// });
+mongoose.connection.on("error", (err) => {
+  console.log(`Mongoose connection error: ${err}`);
+});
 
-// mongoose.connection.on("disconnected", () => {
-//   console.log("Disconnected from DB");
-// });
+mongoose.connection.on("disconnected", () => {
+  console.log("Disconnected from DB");
+});
 
 // process.on("SIGINT", async () => {
 //   mongoose.connection.close(() => {
--- models/contact.js
+++ models/contact.js
@@ -1,9 +1,10 @@
 const { Schema, model } = require("mongoose");
 
 const contactSchema = Schema({
-  name: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  phone: { type: String, required: true, unique: true },
+  name: { type: String, required: [true, "Set name for contact"] },
+  email: { type: String },
+  phone: { type: String },
+  favorite: { type: Boolean, default: false },
   date: { type: Date, default: () => Date.now() },
 });
 
