47bdce585435cc483be0dbe5b34027c8b6a31522
vladislav-slobodenjuk <vladjun@gmail.com>
2022-04-23 16:02:24 +0300

    added db connection

--- config/db.js
+++ config/db.js
@@ -3,10 +3,32 @@ require("dotenv").config();
 
 const DB_HOST = process.env.DB_HOST;
 
-mongoose
+const db = mongoose
   .connect(DB_HOST)
-  .then(console.log("Database connection successful"))
-  .catch((err) => {
-    console.log(err.message);
-    process.exit(1);
-  });
+  .then(console.log("Database connection successful"));
+
+// const db = mongoose.connect(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// mongoose.connection.on("connected", () => {
+//   console.log("Connected to DB");
+// });
+
+// mongoose.connection.on("error", (err) => {
+//   console.log(`Mongoose connection error: ${err}`);
+// });
+
+// mongoose.connection.on("disconnected", () => {
+//   console.log("Disconnected from DB");
+// });
+
+// process.on("SIGINT", async () => {
+//   mongoose.connection.close(() => {
+//     console.log("Disconnected from DB");
+//     process.exit(1);
+//   });
+// });
+
+module.exports = db;
