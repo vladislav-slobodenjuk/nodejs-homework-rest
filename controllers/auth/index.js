9b8760115a4f1aa3ce8781951c233cd99576eabc
vladislav-slobodenjuk <vladjun@gmail.com>
2022-04-27 17:42:36 +0300

    stage to login

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,6 +1,9 @@
 const Errors = require("http-errors");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const SECRET_KEY = process.env.SECRET_KEY;
 // const contactRepository = require("../../repository/contacts");
+
 const { User } = require("../../models/user");
 
 const register = async (req, res) => {
@@ -21,7 +24,30 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  console.log("login");
+  const { email, password } = req.body;
+  const userAtDb = await User.findOne({ email });
+
+  if (!userAtDb) {
+    throw new Errors.Unauthorized("Email or password is wrong");
+  }
+  const passCompare = await bcrypt.compare(password, userAtDb.password);
+
+  if (!passCompare) {
+    throw new Errors.Unauthorized("Email or password is wrong");
+  }
+  console.log(SECRET_KEY);
+  // console.log(userAtDb._id);
+  const token = jwt.sign(userAtDb._id, SECRET_KEY, { expiresIn: "1d" });
+  console.log(token);
+  res.status({
+    status: "success",
+    code: 200,
+    token,
+    user: {
+      email,
+      subscription: userAtDb.subscription,
+    },
+  });
 };
 
 const logout = async (req, res) => {};
